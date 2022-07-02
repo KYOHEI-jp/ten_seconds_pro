2788c69500e56a90c46a9653731fdc35283bcb45
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-07-02 17:05:25 +0900

    todo_card終了

--- lib/widgets/todo_card.dart
+++ lib/widgets/todo_card.dart
@@ -30,7 +30,57 @@ class _TodoCardState extends State<TodoCard> {
         isChecked: widget.isChecked,
         creationDate: widget.creationDate);
     return Card(
-      
+      child: Row(
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Checkbox(
+              value: widget.isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  widget.isChecked = value!;
+                });
+                // anotherTodoのisCheckの値を変更する
+                anotherTodo.isChecked = value!;
+                widget.insertFunction(anotherTodo);
+              },
+            ),
+          ),
+          //タイトルとデータ
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.title,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  widget.creationDate.toString(),
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: const Color(0xFF8F8F8F),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              // delete
+              widget.deleteFunction(anotherTodo);
+            },
+            icon: const Icon(Icons.close),
+          )
+        ],
+      ),
     );
   }
 }
