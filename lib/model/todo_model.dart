e8ec5f170c6b886629dc263b2def3c7c9419469c
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-07-02 17:04:35 +0900

    todo_model追加

---
+++ lib/model/todo_model.dart
@@ -0,0 +1,13 @@
+class Todo {
+  int? id;
+  final String title;
+  DateTime creationDate;
+  bool isChecked;
+
+
+  Todo({
+    this.id,
+    required this.title,
+    required this.creationDate,
+    required this.isChecked
+  });
